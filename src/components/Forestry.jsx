7ccd6259d8170250f319c46c69829e8f51091fd7
AlejandroLiinares <alejandrolinaresnt@gmail.com>
2025-03-28 16:48:01 -0300

    PAGINA FUNCIONAL (antes del css)

--- src/components/Forestry.jsx
+++ src/components/Forestry.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/Services.css';
+import '../styles/Forestry.css';
 
 // Estilos para el componente
 const heroStyle = {
--- src/components/Inspections.jsx
+++ src/components/Inspections.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/Services.css';
+import '../styles/Inspections.css';
 
 // Estilos para el componente
 const heroStyle = {
